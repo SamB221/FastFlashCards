0ec74965749f141d5858879b5f8b0a385b75d145
Sam Bardsley <bardsls@wwu.edu>
2025-03-27 23:33:20 -0700

    Added flip animation

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import './FlashCard.css';
 
 const FlashCard = ({ term, definition, flip}) => {
-    if (!flip) {
-        return (
-            <div id="cardfront">
-                <h1 id="cardtext">{term}</h1>
-            </div>
-        );
-    }
     return (
-        <div id="cardback">
-            <h1 id="cardtext">{definition}</h1>
+        <div id="cardcontainer">
+            <div id="card" className={flip ? 'flip' : ''}>
+                <div id="cardfront">
+                    <h1 id="cardtext">{term}</h1>
+                </div>
+                <div id="cardback">
+                    <h1 id="cardtext">{definition}</h1>
+                </div>
+            </div>
         </div>
     );
 };
