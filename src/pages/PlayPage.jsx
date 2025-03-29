e337f358bdd58e0f8a1d54c8a936ad181bdbb417
Sam Bardsley <bardsls@wwu.edu>
2025-03-28 23:55:32 -0700

    Added ability to switch cards and randomizing function

--- src/pages/PlayPage.jsx
+++ src/pages/PlayPage.jsx
@@ -1,32 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import FlashCard from '../components/FlashCard';
 
 const PlayPage = () => {
     const { id } = useParams();
-    var { set } = JSON.parse(localStorage.getItem(id));
-    //var keypress = new KeyboardEvent();
     const [flip, setFlip] = useState(false);
+    const [index, setIndex] = useState(0);
+    const [sortedSet, setSortedSet] = useState("init");
+    var { set } = JSON.parse(localStorage.getItem(id));
+
+    if (sortedSet === "init") {
+        fisherYatesShuffle(set);
+        setSortedSet(set);
+    } 
+
+
+    function fisherYatesShuffle(arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1)); 
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        } 
+    }
     
     useEffect(() => {
-        // Set up the event listener inside useEffect
         const handleKeyDown = (event) => {
-            if (event.key === " ") {
-                setFlip(prevFlip => !prevFlip); // Toggle flip state
-                console.log(!flip);  // Optional: Check if flip state is toggling correctly
+            if (event.key === " "|| event.key === "ArrowUp" || event.key === "ArrowDown") {
+                setFlip(!flip);
+            } else if (event.key === "ArrowLeft") {
+                if (flip) setFlip(false);
+                if (index > 0) setIndex(index - 1);
+                else setIndex(set.length - 1);
+            } else if (event.key === "ArrowRight") {
+                if (flip) setFlip(false);
+                setIndex((index + 1) % set.length);
             }
         };
 
         document.addEventListener("keydown", handleKeyDown);
 
-        // Cleanup the event listener on component unmount
         return () => {
             document.removeEventListener("keydown", handleKeyDown);
         };
-    }, [flip]);
+    }, [flip, index]);
 
     return (
-        <FlashCard term={set[1].Term} definition={set[1].Definition} flip={flip}/>
+        <FlashCard term={sortedSet[index].Term} definition={sortedSet[index].Definition} flip={flip}/>
     );
 };
 
