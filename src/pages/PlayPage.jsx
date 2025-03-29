0a010c26b7bf2bb9dc72f9f59715fa86bb8dc8a6
Sam Bardsley <bardsls@wwu.edu>
2025-03-29 02:00:27 -0700

    Added confetti

--- src/pages/PlayPage.jsx
+++ src/pages/PlayPage.jsx
@@ -3,12 +3,14 @@ import { useParams } from 'react-router-dom';
 import FlashCard from '../components/FlashCard';
 import UIBar from '../components/UIBar';
 import {FaRandom} from 'react-icons/fa';
+import confetti from 'canvas-confetti';
 import './PlayPage.css'
 
 const PlayPage = () => {
     const { id } = useParams();
     const [flip, setFlip] = useState(false);
     const [index, setIndex] = useState(0);
+    const [totalDone, setTotalDone] = useState(0);
     const [sortedSet, setSortedSet] = useState("init");
     var { set } = JSON.parse(localStorage.getItem(id));
 
@@ -27,21 +29,34 @@ const PlayPage = () => {
     }
 
     function randomizeHelper() {
-        console.log("run");
         if (flip) setFlip(false);
         setIndex(0);
         randomize();
     }
 
     function switchRight() {
+        if (index == set.length-1 && totalDone > set.length-2) triggerConfetti();
         setIndex((index + 1) % set.length);
+        setTotalDone(totalDone + 1);
     }
 
     function switchLeft() {
         if (flip) setFlip(false);
         if (index > 0) setIndex(index - 1);
         else setIndex(set.length - 1);
+        setTotalDone(totalDone + 1);
     }
+
+    const triggerConfetti = () => {
+        confetti({
+          particleCount: 100,
+          spread: 160,
+          origin: { x: 0.5, y: 0.5 },
+          zIndex: -1,
+          duration: 200,
+          disableForReducedMotion: true
+        });
+      }
     
     useEffect(() => {
         const handleKeyDown = (event) => {
@@ -51,14 +66,14 @@ const PlayPage = () => {
             } else if (event.key === "ArrowLeft") {
                 if (flip) {
                     setFlip(false)
-                    setTimeout(switchLeft, 110); // Without delay when flipping, people could cheat!
+                    setTimeout(switchLeft, 50); // Without delay when flipping, people could cheat!
                 } else {
                     switchLeft();
                 }
             } else if (event.key === "ArrowRight") {
                 if (flip) {
                     setFlip(false);
-                    setTimeout(switchRight, 110); // Without delay when flipping, people could cheat!
+                    setTimeout(switchRight, 50); // Without delay when flipping, people could cheat!
                 } else {
                     switchRight();
                 }
