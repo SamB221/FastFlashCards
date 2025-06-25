ac1f672fdd2ffa9e1e19425de608b2cb9b7594ce
Sam Bardsley <bardsls@wwu.edu>
2025-06-25 12:32:18 -0700

    Switching settings to work with Netlify instead of Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
   ],
   server: {
     port: 3000,
+    strictPort: true
   },
 })
