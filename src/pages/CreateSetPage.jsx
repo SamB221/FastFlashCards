8879a573af1174584779098f116116db57457190
Sam Bardsley <bardsls@wwu.edu>
2025-06-26 22:42:46 -0700

    Added skeleton for createSetPage

--- src/pages/CreateSetPage.jsx
+++ src/pages/CreateSetPage.jsx
@@ -2,10 +2,49 @@ import React from 'react';
 import Title from '../components/Title';
 
 const CreateSetPage = () => {
+    const handleSubmit = (event) => {
+        alert("hi");
+        event.preventDefault();
+        const selectedFile = fileInputRef.current.files[0];
+        storeFile(selectedFile);
+    };
+
     return (
         <>
-            <Title title="Back" back="true" />
-            <h1 className="centerText">Set page</h1>
+            <Title title="Create a set" back="true" />
+            <form id="setBuilder" onSubmit={handleSubmit}>
+                <div id="setBuildingBox">
+                    <label htmlFor="setName"><p>Enter the set name</p></label>
+                    <input type="text" id="setName" name="firstname" placeholder="Set name..."></input>
+                    
+                    <div id="cardBuilders">
+                        <div className="cardBuilder">
+                            <div className="smallerBox">
+                                <div className="flex">
+                                    <label htmlFor="term1"><p>Enter a term</p></label>
+                                    <input type="text" id="term1" name="firstname" placeholder="Term..."></input>
+                                </div>
+                                <label htmlFor="def2"><p>Enter a definition</p></label>
+                                <input type="text" id="def2" name="firstname" placeholder="Definition..."></input>
+                            </div>
+                        </div>
+
+                        <div className="cardBuilder">
+                            <div className="smallerBox">
+                                <div className="flex">
+                                    <label htmlFor="term1"><p>Enter a term</p></label>
+                                    <input type="text" id="term1" name="firstname" placeholder="Term..."></input>
+                                </div>
+                                <label htmlFor="def2"><p>Enter a definition</p></label>
+                                <input type="text" id="def2" name="firstname" placeholder="Definition..."></input>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <form className="lowerRight grnBtn" onClick={handleSubmit}>
+                    <input type="button" value="Done"/>
+                </form>
+            </form>
         </>
     );
 };
