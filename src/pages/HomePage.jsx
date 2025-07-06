c0d127bc97cafdb034257dc5b541f4ab6267d7ad
Sam Bardsley <bardsls@wwu.edu>
2025-07-05 22:43:24 -0700

    Updated meta description

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -26,7 +26,7 @@ const HomePage = () => {
 
     return (
         <>
-            <meta name="description" content="An efficient and easy to use flash card website, with dedicated learning and match modes."/> 
+            <meta name="description" content="An efficient and easy to use flash card website, with dedicated learning and match modes. Create a set manually or upload a CSV"/> 
             <Title title={"Your Sets"} />
             <HomeCards />
             <form className="lowerRight grnBtn" onClick={removeSet}>
