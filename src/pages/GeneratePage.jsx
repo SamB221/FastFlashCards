364aab13fa6ddad4ea035bf43b9ac9f54afa7cdf
Sam Bardsley <bardsls@wwu.edu>
2025-07-12 19:50:19 -0700

    Added prompting ability for AI generating feature

--- src/pages/GeneratePage.jsx
+++ src/pages/GeneratePage.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import { React, useState } from 'react';
 import {Link} from 'react-router-dom';
 
 const GeneratePage = () => {
+    const [status, setStatus] = useState("");
+
     async function generate(prompt) {
         try {
             const res = await fetch("/.netlify/functions/generateSet", {
@@ -24,9 +26,36 @@ const GeneratePage = () => {
         }
     }
 
+    async function handleTextForm(e) {
+        let prompt = document.forms['textForm']['prompt'].value;
+        e.preventDefault();
+        setStatus("awaiting");
+
+        const result = await generate(prompt);
+        console.log(result);
+
+        setStatus("completed");
+    }
+
+    if (status === "awaiting") {
+        return (
+            <p>Awaiting answer...</p>
+        );
+    }
+    
+    if (status === "completed") {
+        return (
+            <p>Done!</p>
+        );
+    }
+
     return (
         <>
-            <p>Test</p>
+            <form className="textForm" autoComplete="off" name="textForm" onSubmit={handleTextForm}>
+                <label htmlFor="prompt"><p>Describe the set you would like me to create</p></label>
+                <input type="text" className="textInput1" id="prompt" name="firstname" placeholder="Enter a prompt..."></input>
+                <input type="submit" value="Generate!"></input>
+            </form>
         </>
     );
 };
