16414012139c2fcc6536ac301a1e5daebf1259cc
Sam Bardsley <bardsls@wwu.edu>
2025-09-03 19:11:20 -0700

    Updated database file

--- src/functions/editSet.js
+++ src/functions/editSet.js
@@ -1,5 +1,5 @@
 import { collection, doc, setDoc, getDoc, getDocs } from "firebase/firestore";
-import { db } from '../firebase.js';
+import { db } from '../database.js';
 
 const editSet = {
     async createSet(name, set, user) {
